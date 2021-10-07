6090802c3c45479d64f18d137841ade4a56e1707
Younes Meziane <ys.aameziane@gmail.com>
2021-10-08 00:05:43 +0200

    Ditto

--- lua/advisor-modules/utils/cl_helpers.lua
+++ lua/advisor-modules/utils/cl_helpers.lua
@@ -1,6 +1,10 @@
 Advisor = Advisor or {}
 Advisor.Utils = Advisor.Utils or {}
 
+function Advisor.Utils.LocalizedMessage(ply, color, namespace, key, ...)
+    chat.AddText(color, Advisor.Localization.Localize(namespace, key, unpack({...})))
+end
+
 net.Receive("Advisor.LocalizedMessage", function(ply, len)
     local color = net.ReadColor()
     local namespace = net.ReadString()
@@ -11,8 +15,6 @@ net.Receive("Advisor.LocalizedMessage", function(ply, len)
     for i = 1, count do 
         args[#args + 1] = net.ReadString()
     end
-
-    PrintTable(args)
 
     chat.AddText(color, Advisor.Localization.Localize(namespace, key, unpack(args)))
 end)
