d469fb1fac91735e8ab5460ff9fb2062a51728bd
Younes Meziane <ys.aameziane@gmail.com>
2021-10-07 23:17:14 +0200

    Add CAMI as an Advisor module

---
+++ lua/advisor-modules/cami/module_cami.lua
@@ -0,0 +1,11 @@
+local tbl = 
+{
+    name = "Common Admin Mod Interface (CAMI)",
+    description = "Contains the necessary implementations for interacting with other admin mods and addons.",
+    loadOrder =
+    {
+        "sh_cami.lua",
+    }
+}
+
+return Module(tbl)
