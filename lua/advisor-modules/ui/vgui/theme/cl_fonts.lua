c964ed39c9898dcc5c84ae9120f93a3133f08b83
pilot22 <louis.champi44@gmail.com>
2021-10-13 19:36:46 +0200

    Implemented OnScreenSizeChanged hook & "responsived" fonts

--- lua/advisor-modules/ui/vgui/theme/cl_fonts.lua
+++ lua/advisor-modules/ui/vgui/theme/cl_fonts.lua
@@ -1,43 +1,52 @@
-surface.CreateFont("Rubik.Header", 
-{
-    font = "Rubik",
-    size = 24,
-    antialias = true,
-})
+local function SizeByRatio(x)
+  return x / 2560 * ScrW()
+end
 
-surface.CreateFont("Rubik.Footer",
-{
-    font = "Rubik",
-    size = 20,
-    antialias = true,
-})
+local function GenerateAdvisorFonts()
+  surface.CreateFont("Advisor:Rubik.Header",
+  {
+      font = "Rubik",
+      size = SizeByRatio(32),
+      antialias = true,
+  })
 
-surface.CreateFont("Rubik.Body",
-{
-    font = "Rubik",
-    size = 20,
-    antialias = true,
-})
+  surface.CreateFont("Advisor:Rubik.Footer",
+  {
+      font = "Rubik",
+      size = SizeByRatio(26),
+      antialias = true,
+  })
 
-surface.CreateFont("Rubik.Button",
-{
-    font = "Rubik",
-    size = 20,
-    antialias = true,
-})
+  surface.CreateFont("Advisor:Rubik.Body",
+  {
+      font = "Rubik",
+      size = SizeByRatio(27),
+      antialias = true,
+  })
 
-surface.CreateFont("Advisor.Awesome",
-{
-    font = "Font Awesome 5 Free Solid",
-    antialias = true,
-    extended = true,
-    size = 20,
-})
+  surface.CreateFont("Advisor:Rubik.Button",
+  {
+      font = "Rubik",
+      size = SizeByRatio(24),
+      antialias = true,
+  })
 
-surface.CreateFont("Advisor.SmallAwesome",
-{
-    font = "Font Awesome 5 Free Solid",
-    antialias = true,
-    extended = true,
-    size = 16,
-})
+  surface.CreateFont("Advisor:Awesome",
+  {
+      font = "Font Awesome 5 Free Solid",
+      antialias = true,
+      extended = true,
+      size = SizeByRatio(27),
+  })
+
+  surface.CreateFont("Advisor:SmallAwesome",
+  {
+      font = "Font Awesome 5 Free Solid",
+      antialias = true,
+      extended = true,
+      size = SizeByRatio(23),
+  })
+end
+
+GenerateAdvisorFonts()
+hook.Add("OnScreenSizeChanged", "Advisor:OnScreenSizeChanged:ReGenerateFonts", GenerateAdvisorFonts)
