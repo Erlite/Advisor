bfc8aef93c06bd113a32a6bfb7f3c9915a6f64f2
Younes Meziane <ys.aameziane@gmail.com>
2021-10-16 01:44:52 +0200

    Fix versioning errors

--- lua/advisor/shared.lua
+++ lua/advisor/shared.lua
@@ -1,8 +1,8 @@
 AddCSLuaFile()
 include("moduleloader.lua")
 
-Advisor.CurrentVersion = "0.0.0"
+Advisor.CurrentVersion = "0.1.0"
 Advisor.RepositoryURL = "https://github.com/Erlite/Advisor"
-Advisor.VersionFile = Advisor.RepositoryURL .. "/blob/master/version.txt"
+Advisor.VersionFile = "https://raw.githubusercontent.com/Erlite/Advisor/master/version.txt"
 
 Advisor.Modules.LoadAllModules()
