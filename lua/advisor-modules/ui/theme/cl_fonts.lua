bc959329e14d2af33af9ebec5a8904dbb9877809
Younes Meziane <ys.aameziane@gmail.com>
2021-10-16 22:54:11 +0200

    Test ScreenScale instead of custom function

--- lua/advisor-modules/ui/theme/cl_fonts.lua
+++ lua/advisor-modules/ui/theme/cl_fonts.lua
@@ -1,52 +1,59 @@
-local function SizeByRatio(x)
-  return x / 2560 * ScrW()
-end
+-- local function ScreenScale(x)
+--   return x / 2560 * ScrW()
+-- end
 
 local function GenerateAdvisorFonts()
-  surface.CreateFont("Advisor:Rubik.Header",
-  {
-      font = "Rubik",
-      size = SizeByRatio(24),
-      antialias = true,
-  })
+    surface.CreateFont("Advisor:Rubik.Header",
+    {
+        font = "Rubik",
+        size = ScreenScale(6),
+        antialias = true,
+    })
 
-  surface.CreateFont("Advisor:Rubik.Footer",
-  {
-      font = "Rubik",
-      size = SizeByRatio(20),
-      antialias = true,
-  })
+    surface.CreateFont("Advisor:Rubik.Footer",
+    {
+        font = "Rubik",
+        size = ScreenScale(5),
+        antialias = true,
+    })
 
-  surface.CreateFont("Advisor:Rubik.Body",
-  {
-      font = "Rubik",
-      size = SizeByRatio(22),
-      antialias = true,
-  })
+    surface.CreateFont("Advisor:Rubik.Body",
+    {
+        font = "Rubik",
+        size = ScreenScale(5.5),
+        antialias = true,
+    })
 
-  surface.CreateFont("Advisor:Rubik.Button",
-  {
-      font = "Rubik",
-      size = SizeByRatio(24),
-      antialias = true,
-      weight = 525,
-  })
+    surface.CreateFont("Advisor:Rubik.Button",
+    {
+        font = "Rubik",
+        size = ScreenScale(6),
+        antialias = true,
+        weight = 525,
+    })
 
-  surface.CreateFont("Advisor:Awesome",
-  {
-      font = "Font Awesome 5 Free Solid",
-      antialias = true,
-      extended = true,
-      size = SizeByRatio(20),
-  })
+    surface.CreateFont("Advisor:Rubik.TextEntry", 
+    {
+        font = "Rubik",
+        size = ScreenScale(5),
+        antialias = true,
+    })
 
-  surface.CreateFont("Advisor:SmallAwesome",
-  {
-      font = "Font Awesome 5 Free Solid",
-      antialias = true,
-      extended = true,
-      size = SizeByRatio(16),
-  })
+    surface.CreateFont("Advisor:Awesome",
+    {
+        font = "Font Awesome 5 Free Solid",
+        antialias = true,
+        extended = true,
+        size = ScreenScale(5),
+    })
+
+    surface.CreateFont("Advisor:SmallAwesome",
+    {
+        font = "Font Awesome 5 Free Solid",
+        antialias = true,
+        extended = true,
+        size = ScreenScale(4),
+    })
 end
 
 GenerateAdvisorFonts()
