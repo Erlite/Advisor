7f65ca8a786808bf64f71afc48d2229bcf261dc7
Younes Meziane <ys.aameziane@gmail.com>
2021-10-17 20:55:06 +0200

    [Replicate] Add ValueTable, fix replication issue

--- lua/advisor-modules/replicate/sh_rep_property.lua
+++ lua/advisor-modules/replicate/sh_rep_property.lua
@@ -1,6 +1,39 @@
 RepProperty = {}
 RepProperty.__index = RepProperty
 
+local function FluentAccessorFunc(tab, varname, name, iForce)
+    if ( !tab ) then debug.Trace() end
+
+	tab[ "Get" .. name ] = function( self ) return self[ varname ] end
+
+	if ( iForce == FORCE_STRING ) then
+		tab[ "Set" .. name ] = function( self, v ) 
+            self[ varname ] = tostring( v )
+            return self
+        end
+	return end
+
+	if ( iForce == FORCE_NUMBER ) then
+		tab[ "Set" .. name ] = function( self, v ) 
+            self[ varname ] = tonumber( v )
+            return self
+        end
+	return end
+
+	if ( iForce == FORCE_BOOL ) then
+		tab[ "Set" .. name ] = 
+        function( self, v ) 
+            self[ varname ] = tobool( v )
+            return self
+        end
+	return end
+
+	tab[ "Set" .. name ] = function( self, v )
+        self[ varname ] = v
+        return self
+    end
+end
+
 -- TODO: Add Key/Value type
 ReplicationType = 
 {
@@ -20,7 +53,8 @@ ReplicationType =
     Entity = "Entity", -- An entity (internally, networks the EntIndex() using 16 bits)
     Table = "Table", -- Table properties must also be setup with Replicate, else it'll default to Read/WriteTable. You don't want that, do you?
     List = "List", -- A simple list, considered not ordered. Will be written as an ordinal list.
-    OrderedList = "OrderedList", -- A simple list with ordered numerical keys (first key must be 1)
+    OrderedList = "OrderedList", -- A simple list with ordered numerical keys (first key must be 1),
+    ValueTable = "ValueTable", -- A key -> bool table, will only read and write keys that have true values. Keys are assumed to be strings.
 }
 
 function RepProperty:new(inName, inType)
@@ -41,25 +75,25 @@ function RepProperty:new(inName, inType)
 end
 
 -- Key name of the property
-AccessorFunc(RepProperty, "name", "Name")
+FluentAccessorFunc(RepProperty, "name", "Name")
 -- ReplicationType of the property.
-AccessorFunc(RepProperty, "type", "Type")
+FluentAccessorFunc(RepProperty, "type", "Type")
 -- Amount of bits used for integers
-AccessorFunc(RepProperty, "bits", "Bits")
+FluentAccessorFunc(RepProperty, "bits", "Bits")
 -- The ReplicationType of the value for lists.
-AccessorFunc(RepProperty, "value_type", "ValueType")
+FluentAccessorFunc(RepProperty, "value_type", "ValueType")
 -- The condition in which this property will be written. A single bit will be written to determine whether or not the prop was replicated.
 -- function(tbl), return true if the property should be written.
 -- Only called when writing a table.
-AccessorFunc(RepProperty, "condition", "ReplicationCondition")
+FluentAccessorFunc(RepProperty, "condition", "ReplicationCondition")
 -- If not nil, the RepProperty name this property depends on. It will only be written/read if the dependency's condition is true. 
 -- Only works if the dependency has a replication condition.
-AccessorFunc(RepProperty, "depends_on", "DependsOn")
+FluentAccessorFunc(RepProperty, "depends_on", "DependsOn")
 -- The default value to assign to this property if it does not get replicated (useful for props with a condition or that depend on another)
-AccessorFunc(RepProperty, "default_value", "DefaultValue")
+FluentAccessorFunc(RepProperty, "default_value", "DefaultValue")
 -- The meta table of a table property if any.
 -- Used when reading tables. If nil or unregistered, net.ReadTable() will be used.
-AccessorFunc(RepProperty, "meta_table", "MetaTable")
+FluentAccessorFunc(RepProperty, "meta_table", "MetaTable")
 
 
 function RepProperty:AssertValid()
