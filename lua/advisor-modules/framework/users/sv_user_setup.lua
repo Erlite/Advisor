581925cb66efbd6223b3edb8eb58c40f2049cd09
Younes Meziane <ys.aameziane@gmail.com>
2021-10-17 23:27:55 +0200

    Optimize updating/creating user data

--- lua/advisor-modules/framework/users/sv_user_setup.lua
+++ lua/advisor-modules/framework/users/sv_user_setup.lua
@@ -5,93 +5,6 @@ local players = {}
 
 util.AddNetworkString("Advisor.BroadcastPlayerConnected")
 
-local function OnPlayerDataCreated(success, message, result, affectedRows)
-    if success then
-        Advisor.Log.Info(LogSQL, "Saved player data to database.")
-    else
-        Advisor.Log.Error(LogSQL, "Failed to upsert player data with %s", message)
-    end
-end
-
--- Callback from SetupPlayerData
-local function OnPlayerDataRetrieved(success, message, result, affectedRows, params)
-    if not success then
-        Advisor.Log.Error(LogSQL, "Failed to retrieve player data with %s", message)
-        return 
-    end
-
-    local user = result[1]
-    local ply = player.GetBySteamID64(params["steamid64"])
-    if not ply then return end
-
-    -- Update or insert the player data.
-    local query
-
-    if user then 
-        query =
-        [[
-            UPDATE 'advisor_users'
-            SET last_seen = {{time}}
-            WHERE steamid64 = {{steamid64}}
-        ]]
-    else
-        query =
-        [[
-            INSERT INTO 'advisor_users' (steamid64, joined_at, last_seen)
-            VALUES ({{steamid64}}, {{time}}, {{time}})
-        ]]
-    end
-
-    local params = 
-    {
-        ["time"] = os.time(),
-        ["steamid64"] = ply:SteamID64(),
-    }
-
-    Advisor.SQL.Database:query(query, params, OnPlayerDataCreated)
-
-    local timestamp = 0
-    if user then
-        setmetatable(user, Advisor.User)
-        timestamp = os.time() - user:GetLastSeenAt()
-        Advisor.Log.Info(LogAdvisor, "User '%s' joined, last seen %s ago.", ply:Nick(), 
-            Advisor.Utils.TimestampToReadableText(timestamp))
-    else
-        Advisor.Log.Info(LogSQL, "User '%s' connected for the first time.", ply:Nick())
-        user = Advisor.User(ply)
-    end
-
-    net.Start("Advisor.BroadcastPlayerConnected")
-        net.WriteEntity(ply)
-        net.WriteUInt(timestamp, 32)
-    net.Broadcast()
-
-    hook.Run("Advisor.PlayerDataRetrieved", user)
-end
-
--- Called to setup a player's profile in the database, or update it if it exists.
-local function SetupPlayerData(ply)
-    if not ply or ply:IsBot() then return end
-    players[ply:SteamID64()] = ply
-
-    local query = 
-    [[
-        SELECT * FROM 'advisor_users' 
-        WHERE steamid64={{steamid64}};
-    ]]
-
-    local params = 
-    {
-        ["steamid64"] = ply:SteamID64(),
-    }
-
-    Advisor.SQL.Database:query(query, params, function(s, m, r, a)
-        OnPlayerDataRetrieved(s, m, r, a, params)
-    end)
-end
-
-hook.Add("Advisor.PlayerReady", "AdvisorSetupPlayerData", SetupPlayerData)
-
 local function UpdatePlayerData(ply)
     if not IsValid(ply) or ply:IsBot() then return end
 
@@ -118,3 +31,66 @@ local function UpdatePlayerData(ply)
 end
 
 hook.Add("PlayerDisconnected", "AdvisorUpdatePlayerLastSeenAt", UpdatePlayerData)
+
+local function OnPlayerDataCreated(success, message, result, affectedRows)
+    if not success then
+        Advisor.Log.Error(LogSQL, "Failed to upsert player data with %s", message)
+        return
+    end
+
+    local user = result[1]
+    setmetatable(user, Advisor.User)
+    local ply = player.GetBySteamID64(user:GetSteamID64())
+
+    local timestamp = user:GetLastSeenAt() - user:GetJoinedAt()
+    if timestamp > 0 then
+        timestamp = os.time() - user:GetLastSeenAt()
+        Advisor.Log.Info(LogAdvisor, "User '%s' joined, last seen %s ago.", ply:Nick(), 
+            Advisor.Utils.TimestampToReadableText(timestamp))
+
+        UpdatePlayerData(ply)
+    else
+        Advisor.Log.Info(LogSQL, "User '%s' connected for the first time.", ply:Nick())
+    end
+
+    net.Start("Advisor.BroadcastPlayerConnected")
+        net.WriteEntity(ply)
+        net.WriteUInt(timestamp, 32)
+    net.Broadcast()
+
+    hook.Run("Advisor.PlayerDataRetrieved", user)
+end
+
+-- Called to setup a player's profile in the database, or update it if it exists.
+local function SetupPlayerData(ply)
+    if not ply or ply:IsBot() then return end
+    players[ply:SteamID64()] = ply
+
+    -- Update or insert the player data.
+    local query =
+    [[
+        INSERT INTO advisor_users
+        SELECT {{steamid64}}, {{time}}, {{time}}
+        WHERE NOT EXISTS (SELECT * FROM advisor_users WHERE steamid64={{steamid64}});
+        
+        -- Add the user's name to the aliases if it doesn't exist already.
+        INSERT INTO advisor_user_aliases
+        SELECT {{steamid64}}, {{name}}, {{time}}
+        WHERE NOT EXISTS (SELECT * FROM advisor_user_aliases WHERE steamid64 = {{steamid64}} AND alias = {{name}});
+
+        SELECT * FROM advisor_users
+        WHERE steamid64 = {{steamid64}}
+        LIMIT 1;
+    ]]
+
+    local params = 
+    {
+        ["time"] = os.time(),
+        ["steamid64"] = ply:SteamID64(),
+        ["name"] = ply:Nick(),
+    }
+
+    Advisor.SQL.Database:query(query, params, OnPlayerDataCreated)
+end
+
+hook.Add("Advisor.PlayerReady", "AdvisorSetupPlayerData", SetupPlayerData)
