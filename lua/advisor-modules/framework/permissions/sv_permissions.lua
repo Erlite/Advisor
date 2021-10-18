2d50d8fd9b1c241f0f0837ca62331fb0d1fb4767
Younes Meziane <ys.aameziane@gmail.com>
2021-10-19 01:42:17 +0200

    Implement usergroup & permission loading
    Also loads CAMI registered groups

--- lua/advisor-modules/framework/permissions/sv_permissions.lua
+++ lua/advisor-modules/framework/permissions/sv_permissions.lua
@@ -1,24 +1,79 @@
 Advisor = Advisor or {}
 Advisor.Permissions = Advisor.Permissions or {}
 Advisor.Permissions.Usergroups = Advisor.Permissions.Usergroups or {}
+Advisor.Permissions.UsergroupMap = Advisor.Permissions.UsergroupMap or {}
 
-local function OnUsergroupsRetrieved(success, message, result, affectedRows)
-    PrintTable(result)
+local defaultUsergroups =
+{
+    ["user"] = true,
+    ["admin"] = true,
+    ["superadmin"] = true,
+}
+
+local function OnUsergroupsRetrieved(success, message, results, affectedRows)
+    if not success then 
+        Advisor.Log.Error(LogSQL, "Failed to retrieve usergroups and permissions with error: %s", message)
+        return 
+    end
+
+    for i = 1, #results do
+        -- Check if the name row is in the Usergroups table. If not, we'll add it right now.
+        local data = results[i]
+        if not Advisor.Permissions.Usergroups[data["name"]] then
+            local newGroup = Advisor.Usergroup(data)
+            Advisor.Permissions.Usergroups[#Advisor.Permissions.Usergroups + 1] = newGroup
+            Advisor.Permissions.UsergroupMap[newGroup:GetName()] = newGroup
+
+            -- Check that the usergroup isn't in CAMI already.
+            -- If it is, flag it as partial_control
+            local registeredGroup = CAMI.GetUsergroup(newGroup:GetName()) 
+            if registeredGroup and registeredGroup.CAMI_Source ~= "Advisor" then
+                newGroup:SetPartialControl(true)
+                newGroup:SetSource(registeredGroup.CAMI_Source or "Unknown")
+            end
+
+            -- Notify CAMI of the newly added usergroup.
+            if not defaultUsergroups[newGroup:GetName()] then
+                CAMI.RegisterUsergroup(newGroup:GetCAMIUsergroup(), "Advisor")
+            end
+        end
+
+        -- Apply permission if there is one.
+        if data["permission"] then
+            local group = Advisor.Permissions.UsergroupMap[data["name"]]
+            group:SetPermission(data["permission"], data["value"] ~= "0")
+        end
+    end
+
+    -- Grab all usergroups registered on CAMI
+    local camiGroups = CAMI.GetUsergroups()
+    for i = 1, #camiGroups do
+        local camiGroup = camiGroups[i]
+        if not Advisor.Permissions.Usergroups[camiGroup.Name] then
+            local thirdPartyGroup = Advisor.Usergroup()
+            thirdPartyGroup:SetName(camiGroup.Name)
+            thirdPartyGroup:SetInherits(camiGroup.Inherits)
+            thirdPartyGroup:SetSource(camiGroup.CAMI_Source)
+
+            Advisor.Permissions.Usergroups[#Advisor.Permissions.Usergroups + 1] = thirdPartyGroup
+            Advisor.Permissions.UsergroupMap[camiGroup.Name] = thirdPartyGroup
+        end
+    end
 end
 
 local function InitializeUsergroups()
     local query =
     [[
         -- Add the user, admin and superadmin usergroups if they've been forcefully deleted, or just don't exist yet.
-        INSERT INTO advisor_usergroups
+        INSERT INTO advisor_usergroups(name, display_name, color, can_delete, inherits)
         SELECT 'user', 'User', 4294967295, 0, 'user'
         WHERE NOT EXISTS (SELECT * FROM advisor_usergroups WHERE name='user');
 
-        INSERT INTO advisor_usergroups
+        INSERT INTO advisor_usergroups(name, display_name, color, can_delete, inherits)
         SELECT 'admin', 'Administrator', 4294967295, 0, 'user'
         WHERE NOT EXISTS (SELECT * FROM advisor_usergroups WHERE name='admin');
 
-        INSERT INTO advisor_usergroups
+        INSERT INTO advisor_usergroups(name, display_name, color, can_delete, inherits)
         SELECT 'superadmin', 'Super Administrator', 4294967295, 0, 'admin'
         WHERE NOT EXISTS (SELECT * FROM advisor_usergroups WHERE name='superadmin');
 
@@ -28,14 +83,16 @@ local function InitializeUsergroups()
         WHERE name = 'user' OR name = 'admin' OR name = 'superadmin';
 
         SELECT * FROM advisor_usergroups
-        LEFT JOIN advisor_usergroup_permissions ON advisor_usergroups.name = advisor_usergroup_permissions.usergroup_name;
+        LEFT JOIN advisor_usergroup_permissions USING(name);
     ]]
 
     Advisor.SQL.Database:query(query, {}, OnUsergroupsRetrieved)
 end
 
-if Advisor.SQL.IsInitialized then
-    InitializeUsergroups()
+if Advisor.SQL and Advisor.SQL.IsInitialized then
+    if #Advisor.Permissions.Usergroups == 0 then
+        InitializeUsergroups()
+    end
 else
     hook.Add("Advisor.DatabaseReady", "Advisor.InitializePermissions", InitializeUsergroups)
 end
